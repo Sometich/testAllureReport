efbfc05d85d7948bb74f65aa15e1f248fc440d5c
Алексей Балаян <alexey.balayan@icloud.com>
2023-02-05 18:35:04 +0300

    result

---
+++ src/test/java/TestReport.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestReport {
+    @Test
+    public void test1() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void test2() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void test3() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void test4() {
+        Assertions.assertTrue(true);
+    }
+}
